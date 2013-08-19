cf60eab7b74df3ffe48de9b6f0f9929b97675866
Michael Cailler <cailler.michael@gmail.com>
2013-08-19 16:04:49 -0400

    Update spout tests to be more stringent

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -3,14 +3,35 @@ require 'test_helper'
 class DictionaryTest < Test::Unit::TestCase
   include Spout::Tests
 
-  # You may add additional tests here
-  # def test_truth
-  #   assert true
-  # end
+  VALID_UNITS = ['','cm','%','mmHG','months','years'] #Example ['mmHG','bpm','readings','minutes','%','hours','MET']
 
-  def test_variable_uniqueness
-    files = Dir.glob("variables/**/*.json").collect{|file| file.split('/').last }
-    assert_equal [], files.select{ |f| files.count(f) > 1 }.uniq
+  def assert_units(units, msg = nil)
+    full_message = build_message(msg, "? invalid units. Valid types: #{VALID_UNITS.join(', ')}", units)
+    assert_block(full_message) do
+      VALID_UNITS.include?(units)
+    end
   end
 
+  def assert_domain_present(domain_name, msg = nil)
+    full_message = build_message(msg, "Variables of type choices need to specify a domain")
+    assert_block(full_message) do
+      domain_name != nil
+    end
+  end
+
+  Dir.glob("variables/**/*.json").each do |file|
+    if ['numeric','integer'].include?(json_value(file, :type))
+      define_method("test_units: "+file) do
+        assert_units json_value(file, :units)
+      end
+    end
+  end
+
+  Dir.glob("variables/**/*.json").each do |file|
+    if json_value(file, :type) == "choices"
+      define_method("test_domain_present:"+file) do
+        assert_domain_present json_value(file, :domain)
+      end
+    end
+  end
 end
