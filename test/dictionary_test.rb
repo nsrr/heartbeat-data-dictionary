fd67414e2ffcc2bb5d408d97c52f52683b3682b3
Michael Cailler <cailler.michael@gmail.com>
2014-06-18 10:11:54 -0400

    Update valid units to be plural

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -9,7 +9,7 @@ class DictionaryTest < Minitest::Test
   # iterators that can be used to write custom tests
   include Spout::Helpers::Iterators
 
-  VALID_UNITS = ['readings','%','milliseconds','','hours','events per hour','events','mmHG','minutes','seconds','bpm','cm','years old','days per week','naps','ovaries','periods','cigarettes','kg','ft','in','lb'] #Example ['mmHG','bpm','readings','minutes','%','hours','MET']
+  VALID_UNITS = ["", "beats per minute", "centimeters", "cigarettes", "days per week", "desaturations per hour", "events", "events per hour", "feet", "hours", "inches", "kilograms", "millimeters of mercury", "milliseconds", "minutes", "naps", "ovaries", "percent", "periods", "pounds", "readings", "seconds", "years"]
 
   @variables.select{|v| ['numeric','integer'].include?(v.type)}.each do |variable|
     define_method("test_units: "+variable.path) do
